d8aaf92222b498cca4b556d6f52caa8c5c59e25e
Erick Vieira <erick-vieira-s@outlook.com>
2017-09-25 14:37:57 -0300

    Pronto para migrar

--- test/js/index.js
+++ test/js/index.js
@@ -63,8 +63,8 @@ $(document).ready(function() {
     scrollBar: true,
     responsiveWidth: 400,
     navigation: true,
-    navigationTooltips: ['home', 'about', 'portfolio', 'contact', 'connect'],
-    anchors: ['home', 'about', 'portfolio', 'contact', 'connect'],
+    navigationTooltips: ['home', 'about', 'portfolio', 'contact', 'faq', 'connect'],
+    anchors: ['home', 'about', 'portfolio', 'contact', 'faq', 'connect'],
     menu: '#myMenu',
     fitToSection: false,
 
