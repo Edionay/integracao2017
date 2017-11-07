609669b0df84f088c61244c007a63ed69cf6dcfd
Edionay <edionay@gmail.com>
2017-11-07 20:42:04 -0200

    Listagem por período.

--- src/app/disciplinas/disciplinas.component.ts
+++ src/app/disciplinas/disciplinas.component.ts
@@ -10,6 +10,16 @@ import { Response } from '@angular/http';
 export class DisciplinasComponent implements OnInit {
   disciplinas: Disciplina[];
   disciplinaQueVaiAparecerNoModal: Disciplina;
+  periodo1: Disciplina[];
+  periodo2: Disciplina[];
+  periodo3: Disciplina[];
+  periodo4: Disciplina[];
+  periodo5: Disciplina[];
+  periodo6: Disciplina[];
+  periodo7: Disciplina[];
+  periodo8: Disciplina[];
+  periodo9: Disciplina[];
+
   constructor(private dadosJson: DadosService) {
     this.disciplinas = [];
     this.disciplinaQueVaiAparecerNoModal = {
@@ -29,6 +39,17 @@ export class DisciplinasComponent implements OnInit {
   ngOnInit() {
     this.dadosJson.baixarDados().subscribe((response: Response) => {
       this.disciplinas = response.json().disciplina;
+      this.periodo1 = this.disciplinas.slice(0, 4);
+      this.periodo2 = this.disciplinas.slice(4, 9);
+      this.periodo3 = this.disciplinas.slice(9, 14);
+      this.periodo4 = this.disciplinas.slice(14, 19);
+      this.periodo5 = this.disciplinas.slice(19, 23);
+      this.periodo6 = this.disciplinas.slice(23, 27);
+      this.periodo7 = this.disciplinas.slice(27, 32);
+      this.periodo8 = this.disciplinas.slice(32, 37);
+      this.periodo9 = this.disciplinas.slice(37);
+
+      console.log(this.periodo1);
     });
   }
   gerarDisciplinaModal(posicao: number): void {
