7ec4c80b1ab3daffc38c7d553deb213a4099b872
Edionay <edionay@gmail.com>
2017-11-12 15:05:48 -0200

    Auto-generated commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,7 @@
-$(document).ready(function(){
-  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+$(document).ready(function () {
   $('.modal').modal();
+  $(".button-collapse").sideNav();
+  $('.scrollspy').scrollSpy(
+    {scrollOffset: 50}
+  );
 });
