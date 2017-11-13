98d9f5707f32cca86aca76b211a84f92743397db
Edionay <edionay@gmail.com>
2017-11-12 22:20:54 -0200

    Busca de disciplinas funcionando.

--- src/app/busca/busca.component.ts
+++ src/app/busca/busca.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import 'rxjs/add/operator/map';
+import {Disciplina, disciplinas, Topico, topicosPpc} from '../../assets/data/engenhariaDeSoftware';
 
 @Component({
   selector: 'app-busca',
   templateUrl: './busca.component.html',
   styleUrls: ['./busca.component.css']
 })
-export class BuscaComponent {
+export class BuscaComponent implements OnInit {
+  disciplinas = disciplinas;
+  topicos = topicosPpc;
+  disciplinasFiltradas = this.disciplinas;
+  topicosFiltrados = this.topicos;
 
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+  mudarValor(termo: string) {
+    this.disciplinasFiltradas = this.disciplinas;
+    this.disciplinasFiltradas = this.disciplinasFiltradas.filter(disciplina => {
+        return disciplina.nome.includes(termo)
+          || disciplina.ementa.some(item => item.includes(termo))
+          || disciplina.bibliografia.some(item => item.includes(termo));
+      });
+  }
 }
